92bde282b9bf9b491309cbaf83891db0a37205c3
DivamJandial <divam.15-cse-18@mietjammu.in>
2021-05-07 18:47:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import {Footer} from "./MyComponents/Footer";
+import {Todos} from "./MyComponents/Todos";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import {AddTodo} from "./MyComponents/AddTodo";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")===null){
+    initTodo= [];
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo)=>{
+    
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc)=>{
+    
+    let sno;
+    if(todos.length===0){
+      sno = 1;
+    }
+    else{  
+      sno = todos[todos.length-1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    
+  }, [todos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Header title="My Todos List" searchBar={false}/>
+    <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+              <>
+               <AddTodo addTodo= {addTodo}/>
+               <Todos todos={todos} onDelete={onDelete}/>
+               </>
+            )
+          }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+         </Switch>
+   
+    <Footer/>
+    </Router>
+
+    
+
+    </>
   );
 }
 
 export default App;
+
---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className = "container my-3">
+            This is an about component.
+            <p className=" py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi magnam, consequatur voluptate dolorum esse quibusdam rem alias, perferendis unde facere libero. Sunt quisquam eum veritatis, aliquid dolore deleniti repellendus ea deserunt qui, iure vero.</p>
+        </div>
+    )
+}
+ 
